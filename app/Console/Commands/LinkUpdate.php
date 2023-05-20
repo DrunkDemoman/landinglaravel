c64acc79e08abaeb35feacd46bbe19f6ea9b3cfd
Ferdinand Putra Gumilang Silalahi <gumilangsilalahi3@gmail.con>
2023-05-20 16:34:48 +0700

    added link update command

--- app/Console/Commands/LinkUpdate.php
+++ app/Console/Commands/LinkUpdate.php
@@ -13,7 +13,7 @@ class LinkUpdate extends Command
      *
      * @var string
      */
-    protected $signature = 'app:link-update {link_id}';
+    protected $signature = 'link:update {link_id}';
 
     /**
      * The console command description.
@@ -32,7 +32,28 @@ class LinkUpdate extends Command
 
         if ($link == null)
         {
-            $this->error("Invalid or non-ecistent link ID.")
+            $this->error("Invalid or non-ecistent link ID.");
+            return 1;
         }
+        $link->description = $this->ask('Link Description (ENTER to keep current)') ?? $link->description;
+        $list_name = $this->ask('Link List (ENTER to keep current)') ?? $link->link_list->title;
+
+        $this->info("Description: $link->description");
+        $this->info("Listed in: " . $list_name);
+ 
+        if ($this->confirm('Is this information correct?')) {
+            $list = LinkList::firstWhere('slug', $list_name);
+            if (!$list) {
+                $list = new LinkList();
+                $list->title = $list_name;
+                $list->slug = $list_name;
+                $list->save();
+            }
+            $link->link_list()->associate($list)->save();
+            $this->info("Updated.");
+        }
+
+        return 0;
+
     }
 }
